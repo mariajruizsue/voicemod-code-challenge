de29ae8048fb443c15037f5145213f260cb68d49
Maria Jesús Ruiz <mariajesusruiz@gmail.com>
2022-01-17 19:27:27 +0100

    [components] Add list of voices component
    Signed-off-by: Maria Jesús Ruiz <mariajesusruiz@gmail.com>

---
+++ src/components/ListOfVoices/ListOfVoices.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Voice } from "../Voice/Voice";
+import './ListOfVoices.css'
+
+export function ListOfVoices ({voices}) {
+  return (
+    <>
+    <div className='list-voices'>
+      {
+        voices.map(({id, name, icon, tags}) =>
+          <Voice
+            id={id}
+            key={id}
+            name={name}
+            icon={icon}
+            tags={tags}
+          />
+        )
+      }
+    </div>
+    </>
+  );
+}
