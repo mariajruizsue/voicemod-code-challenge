348a3f666c7ea2f31eb8752a856af154fb55ebbf
Maria Jesús Ruiz <mariajesusruiz@gmail.com>
2022-01-17 19:27:06 +0100

    [components] Add voice component
    Signed-off-by: Maria Jesús Ruiz <mariajesusruiz@gmail.com>

---
+++ src/components/Voice/Voice.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useGlobalState } from 'state-pool';
+import { Fav } from "../Fav/Fav";
+
+import './Voice.css'
+
+
+export function Voice ({ id, name, icon, tags }) {
+  const [voiceSelected, setVoiceSelected] = useGlobalState("voiceSelected");
+
+  const handlerClick = () => {
+    setVoiceSelected({ id, name, icon, tags });
+  }
+
+  return (
+    <div className={`voice 
+    ${voiceSelected.id === id ? 'active' : 'inactive'}`}>
+      <div className='voice-fav-button'> 
+        <Fav id={id} name={name} icon={icon} tags={tags}></Fav>
+      </div>
+      <button className='select-button' onClick={handlerClick}>
+        <img className='voice-icon' loading='lazy' alt={`Icon ${name}`} src={`../assets/${icon}`} />
+        <h4 className="voice-name">{name}</h4>
+      </button>
+    </div>
+  )
+};
